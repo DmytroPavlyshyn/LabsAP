ba25d5d1c0e851981223749f50317a9b3074b733
Dmytro <dimapavlyshyn@gmail.com>
2018-09-18 19:17:48 +0300

    Added Task 10

---
+++ Lab1/Task10/src/com/pavlyshyn/Main.java
@@ -0,0 +1,10 @@
+package com.pavlyshyn;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    for(int i = 0; i < 3; i++ ){
+            System.out.println("agrgs["+i+"] = "+args[i]);
+        }
+    }
+}
