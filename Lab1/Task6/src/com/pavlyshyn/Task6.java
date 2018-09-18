5ab8f2851c28a1cdbc3346e5b8a0870c720429a4
Dmytro <dimapavlyshyn@gmail.com>
2018-09-18 20:11:03 +0300

    Added Task 6

---
+++ Lab1/Task6/src/com/pavlyshyn/Task6.java
@@ -0,0 +1,11 @@
+package com.pavlyshyn;
+
+public class Task6 {
+    int storage(String s) {
+        return s.length() * 2;
+    }
+    public static void main(String[] args) {
+	    Task6 obj = new Task6();
+        System.out.println(obj.storage("Task 6"));
+    }
+}
