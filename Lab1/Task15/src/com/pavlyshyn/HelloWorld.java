1fcc83f0a1cea478f5136401f90bba444217203f
Dmytro <dimapavlyshyn@gmail.com>
2018-09-18 23:08:43 +0300

    Added Task 13, 14, 15, 16

---
+++ Lab1/Task15/src/com/pavlyshyn/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.pavlyshyn;
+//:HelloWorld.java
+/**
+ * @author Dmytro Pavlyshyn
+ * @version 1.0
+ *
+ * */
+public class HelloWorld {
+    /** Entry point to class & application.
+     * @param args array of string arguments (optional)
+     * @throws exceptions No exceptions thrown
+     */
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+} /* Output: (100% match)
+HelloWorld
+*///:~
--- Lab1/Task2/src/com/pavlyshyn/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.pavlyshyn;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld");
-    }
-}
